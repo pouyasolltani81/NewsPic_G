922c9deaa17b7a46033ca9600f993735e5965f16
pouyaWork <pouyasoltani81@gmail.com>
2025-08-12 10:28:17 +0330

    test collect

--- BackBoiler/src/app/settings.py
+++ BackBoiler/src/app/settings.py
@@ -200,7 +200,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -223,11 +223,11 @@ SPECTACULAR_SETTINGS = {
 
 
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     BASE_DIR / "ui" /"static",
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "ui" /"static",
     
-# ]
+]
 
 # If you're using collectstatic for production
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
